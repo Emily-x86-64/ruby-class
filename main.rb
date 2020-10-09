f170e434bbd9f9621442708751a671782af858d9
Filia Medici <69800575+Filia-Medici-x86-64@users.noreply.github.com>
2020-10-09 13:19:30 -0400

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,6 @@
+name = "Sarah"
+age = "25"
+puts "There was once a woman named " + name
+puts "She was " + age +  " years old"
+puts "She liked the name " + name
+puts "but she hated being " + age +  " years old"
